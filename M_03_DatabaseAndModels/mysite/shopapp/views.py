ed46fd299ecf6a81d5a4df6d32f1b5f8b32c146b
Mikhail Nartsissov <nartsissov_ma@mail.ru>
2023-05-18 15:58:45 +0300

    0005-shopapp-created-again-in-module-3

---
+++ M_03_DatabaseAndModels/mysite/shopapp/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+from timeit import default_timer
+
+
+def shop_index(request: HttpRequest) -> HttpResponse:
+    products = [
+        ("Приворотное зелье", 1999,
+         "https://i.pinimg.com/236x/32/72/39/3272398743d01e8b426dc469210cc177.jpg", True, None),
+        ("Настойка от сглаза", 999,
+         "https://i.pinimg.com/236x/a6/3d/57/a63d57a92465271c6f6fbf6899cc7b54.jpg", False, True),
+        ("Эликсир мудрости", 2999,
+         "https://i.pinimg.com/236x/b8/77/b3/b877b3872db9e844c2d9942a396e277c.jpg", None, False),
+    ]
+    properties = [
+        'Эксклюзивный', 'Контрабандный', 'Уникальный',
+    ]
+    price_adv = [
+        'Дешевле нет нигде во Вселенной!', 'Лучшей цены вам не найти!', 'Так дёшево бывает только в сказке!',
+    ]
+    context = {
+        "products": products,
+        "properties": properties,
+        "price_adv": price_adv,
+        "time_running": default_timer(),
+    }
+    return render(request, "shopapp/shop-index.html", context=context)
