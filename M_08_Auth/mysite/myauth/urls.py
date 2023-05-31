0a48000a8042190e958a279867d4b8cbebfe0386
Mikhail Nartsissov <nartsissov_ma@mail.ru>
2023-05-31 14:23:28 +0300

    00023-class-based-login-view-and-redefined-logout-view-added

---
+++ M_08_Auth/mysite/myauth/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView
+from .views import (
+    MyLogoutView,
+)
+
+
+app_name = "myauth"
+urlpatterns = [
+    path("login/", LoginView.as_view(
+        template_name='myauth/login.html',
+        redirect_authenticated_user=True
+    ), name='login'),
+    path("logout/", MyLogoutView.as_view(), name='logout'),
+]
