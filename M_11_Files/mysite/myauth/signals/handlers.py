85fc59bcc55ad32dc247efce22c94b6385c1e5f5
Mikhail Nartsissov <nartsissov_ma@mail.ru>
2023-06-21 15:50:44 +0300

    00041-removing-an-old-avatar-using-signals-made

---
+++ M_11_Files/mysite/myauth/signals/handlers.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import pre_save
+from django.dispatch import  receiver
+from os import remove
+
+from ..models import Profile
+
+
+@receiver(pre_save, sender=Profile)
+def pre_save_handler(sender, instance, **kwargs):
+    if instance.avatar:
+        old_image_uid = instance.user_id
+        old_image = sender.objects.get(user_id=old_image_uid)
+        if old_image.avatar and not old_image.avatar.path == instance.avatar.path:
+            old_image_path = old_image.avatar.path
+            print(old_image_path)
+            remove(old_image_path)
