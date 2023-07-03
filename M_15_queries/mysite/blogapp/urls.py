5dee9757a998cbab3a4e5542cb548e69bd0e4a85
Mikhail Nartsissov <nartsissov_ma@mail.ru>
2023-07-03 19:03:31 +0300

    00047-CategoryStatisticsView-made-annotate-and-Count-and-Max-used

--- M_15_queries/mysite/blogapp/urls.py
+++ M_15_queries/mysite/blogapp/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from .views import (
     ArticleListView,
     ArticleTextView,
+    CategoryStatisticsView,
 )
 
 app_name = "blogapp"
@@ -12,4 +13,5 @@ app_name = "blogapp"
 urlpatterns = [
     path("", ArticleListView.as_view(), name="index"),
     path("<int:pk>/", ArticleTextView.as_view(), name="article_text"),
+    path("stat/", CategoryStatisticsView.as_view(), name="category_statistics"),
 ]
--- M_15_queries/mysite/blogapp/views.py
+++ M_15_queries/mysite/blogapp/views.py
@@ -1,5 +1,6 @@
-from .models import Article
+from .models import Article, Category, Author
 from django.views.generic import ListView, DetailView
+from django.db.models import Count, Min, Sum, Max, F
 
 
 class ArticleListView(ListView):
@@ -16,3 +17,12 @@ class ArticleTextView(DetailView):
         Article.objects.values("title", "content")
     )
     context_object_name = "article_text"
+
+
+class CategoryStatisticsView(ListView):
+    template_name = "blogapp/category_statistics.html"
+    queryset = Category.objects.values("name").annotate(
+        authors_count=Count("article__author", distinct=True),
+        articles_count=Count("article"),
+        top_author=Max("article__author__name"),
+    )
